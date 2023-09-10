9d0987a9239e2b4a187e28acfa37343f13ce514b
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-10 16:06:28 -0500

    working on the styling

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ selectionBtn.addEventListener('click', (e, movies) => {
     (movies) => {
       const display = document.querySelector('.display')
       display.style.display = 'flex'
+      const locationName = document.querySelector('.locationName')
       const mainImage = document.createElement('img')
       const title = document.createElement('h1')
       const temp = document.createElement('h1')
       const location = document.createElement('p')
-      location.textContent = `Currently in ${movies.location.name}`
+      location.textContent = `${movies.location.name}`
       temp.textContent = `${movies.current.temp_f} `
       title.textContent = movies.current.condition.text
       mainImage.src = movies.current.condition.icon
-      display.appendChild(location)
+      locationName.appendChild(location)
       display.appendChild(mainImage)
      
       display.appendChild(title)
