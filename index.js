4e4f036c2c03ec621822a2ae87c81ddf5c876574
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-10 13:59:17 -0500

    formated the display

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ selectionBtn.addEventListener('click', (e, movies) => {
       const mainImage = document.createElement('img')
       const title = document.createElement('h1')
       const temp = document.createElement('h1')
+      const location = document.createElement('p')
+      location.textContent = movies.location.name
       temp.textContent = `${movies.current.temp_f} `
       title.textContent = movies.current.condition.text
       mainImage.src = movies.current.condition.icon
+      display.appendChild(location)
       display.appendChild(mainImage)
-      display.appendChild(temp)
+     
       display.appendChild(title)
+      display.appendChild(temp)
       
     }
   )
