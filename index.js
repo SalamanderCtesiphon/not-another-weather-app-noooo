0016edc343c7e1982b5d34b69199701db87a0502
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-10 12:40:29 -0500

    implement a form element to allow the user to enter their city or zip code for a forecast query

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('hi')
+
+async function getWeather() {
+  const response = await 
+    fetch(`https://api.weatherapi.com/v1/current.json?key=11111111111111111&q=${location}`);
+  const movies = await response.json();
+  console.log(movies);
+}
