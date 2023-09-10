ef959bb5f21271cee939eafbf0e4400b5b380b33
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-10 13:13:29 -0500

    had to move an element back inside of a function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const selectionBtn = document.getElementById('selection-btn')
-let selection = document.getElementById('selection').value
+
 
 selectionBtn.addEventListener('click', (e) => {
   e.preventDefault()
@@ -9,32 +9,11 @@ selectionBtn.addEventListener('click', (e) => {
 })
 
 async function getWeather() {
-  
+  let selection = document.getElementById('selection').value
   const response = await 
-    fetch(`https://api.weatherapi.com/v1/current.json?key=6198ff01fbf84dfbbac171056230305&q=${selection}`, { mode: 'cors'});
+    fetch(`https://api.weatherapi.com/v1/current.json?key=6198ff01fbf84dfbbac171056230305&q=${selection}`,
+      { mode: 'cors'});
   const movies = await response.json();
   console.log(movies);
   return movies
 }
-
-
-
-
-
-/* (e) => {
-  e.preventDefault()
-  const selection = document.getElementById('selection').value
-
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=FKHrnl8qaMWyeJf85XwRk8AKF6GmD3Zr&s=${selection}`, {
-  mode: 'cors'
-})
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url
-  })
-  .catch(function(err) {
-    console.log(err)
-  })
-} */
