0f9da3639f955365eba058835a8a45a59cde4f51
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-11 12:03:42 -0500

    exracted out the page render logic into a function and got that function to work

--- index.js
+++ index.js
@@ -3,100 +3,102 @@ const selectionBtn = document.getElementById('selection-btn')
 const currentYear = document.querySelector('.year');
 currentYear.textContent = new Date().getFullYear();
 
+var fahrenheit = true
+
 selectionBtn.addEventListener('click', (e) => {
   e.preventDefault()
 
   const form = document.getElementById('form')
   form.style.display = 'none'
 
-  var fahrenheit = true
-
   getWeather()
   .then(
     (movies) => {
-      const display = document.querySelector('.display')
-      display.style.display = 'flex'
-      const locationName = document.querySelector('.locationName')
-      const cardBody = document.querySelector('.cardBody')
-      const mainImage = document.createElement('img')
-      const title = document.createElement('h1')
-      title.setAttribute('class', 'titleName')
-      const temp = document.createElement('h1')
-      const tempBtn = document.createElement('button')
-      tempBtn.textContent = 'change units'
-      tempBtn.setAttribute('class', 'button')
-
-      tempBtn.addEventListener('click', (e) => {
-        fahrenheit = !fahrenheit
-        console.log(fahrenheit)
-        window.location = window.location
-        getWeather()
-      })
-
-      const location = document.createElement('p')
-      const dateTime = document.querySelector('.dateTime')
-      dateTime.textContent = movies.location.localtime
-      
-      location.textContent = `${movies.location.name}`
-      temp.textContent = `${movies.current.temp_f} F`
-      if(movies.current.condition.text === "Sunny") {
-        mainImage.src = '/icons/sunny.png'
-      } else {
-        mainImage.src = movies.current.condition.icon
-        mainImage.style.height = '80px'
-      }
-
-      title.textContent = movies.current.condition.text
-      const cardBodyBox = document.createElement('div')
-      cardBodyBox.style.textAlign = 'center'
-
-      const forcastDisplay = document.querySelector('.forcastDisplay')
-      forcastDisplay.style.display = 'flex'
-
-      const hiTemp = document.getElementById('hiTemp')
-      hiTemp.textContent = movies.forecast.forecastday[0].day.maxtemp_f
-      const lowTemp = document.getElementById('lowTemp')
-      lowTemp.textContent = movies.forecast.forecastday[0].day.mintemp_f
-      const sunUp = document.getElementById('sunup')
-      sunUp.textContent = movies.forecast.forecastday[0].astro.sunrise
-      const sunDown = document.getElementById('sundown')
-      sunDown.textContent = movies.forecast.forecastday[0].astro.sunset
-
-      const nextDayImg = document.querySelector('.ndImage')
-      const ndTemp = document.querySelector('.ndTemp')
-      ndTemp.textContent = movies.forecast.forecastday[1].day.maxtemp_f
-      const ndimg = document.createElement('img')
-      ndimg.src = movies.forecast.forecastday[1].day.condition.icon
-      ndimg.style.height = '60px'
-      
-      nextDayImg.appendChild(ndimg)
-      const ndText = document.querySelector('.ndText')
-      ndText.textContent = movies.forecast.forecastday[1].day.condition.text
-
-      const dayAfterImg = document.querySelector('.daImage')
-      const daTemp = document.querySelector('.daTemp')
-      daTemp.textContent = movies.forecast.forecastday[2].day.maxtemp_f
-      const daimg = document.createElement('img')
-      daimg.src = movies.forecast.forecastday[2].day.condition.icon
-      daimg.style.height = '60px'
-      
-      dayAfterImg.appendChild(daimg)
-      const daText = document.querySelector('.daText')
-      daText.textContent = movies.forecast.forecastday[2].day.condition.text
-
-      locationName.appendChild(location)
-      cardBody.appendChild(mainImage)
-      cardBodyBox.appendChild(title)
-      cardBodyBox.appendChild(temp)
-      cardBody.appendChild(cardBodyBox)
-      cardBody.appendChild(tempBtn)
-      display.appendChild(cardBody) 
-    }
-  )
-  .catch(function(err) {
+      renderScreen(movies)
+  }).catch(function(err) {
     console.log(err)
   })
 })
+
+function renderScreen(movies) {
+  
+  const display = document.querySelector('.display')
+  display.style.display = 'flex'
+  const locationName = document.querySelector('.locationName')
+  const cardBody = document.querySelector('.cardBody')
+  const mainImage = document.createElement('img')
+  const title = document.createElement('h1')
+  title.setAttribute('class', 'titleName')
+  const temp = document.createElement('h1')
+  const tempBtn = document.createElement('button')
+  tempBtn.textContent = 'change units'
+  tempBtn.setAttribute('class', 'button')
+
+  tempBtn.addEventListener('click', (e) => {
+    fahrenheit = !fahrenheit
+    console.log(fahrenheit)
+  })
+
+  const location = document.createElement('p')
+  const dateTime = document.querySelector('.dateTime')
+  dateTime.textContent = movies.location.localtime
+  
+  location.textContent = `${movies.location.name}`
+  temp.textContent = `${movies.current.temp_f} F`
+  if(movies.current.condition.text === "Sunny") {
+    mainImage.src = '/icons/sunny.png'
+  } else {
+    mainImage.src = movies.current.condition.icon
+    mainImage.style.height = '80px'
+  }
+
+  title.textContent = movies.current.condition.text
+  const cardBodyBox = document.createElement('div')
+  cardBodyBox.style.textAlign = 'center'
+
+  const forcastDisplay = document.querySelector('.forcastDisplay')
+  forcastDisplay.style.display = 'flex'
+
+  const hiTemp = document.getElementById('hiTemp')
+  hiTemp.textContent = movies.forecast.forecastday[0].day.maxtemp_f
+  const lowTemp = document.getElementById('lowTemp')
+  lowTemp.textContent = movies.forecast.forecastday[0].day.mintemp_f
+  const sunUp = document.getElementById('sunup')
+  sunUp.textContent = movies.forecast.forecastday[0].astro.sunrise
+  const sunDown = document.getElementById('sundown')
+  sunDown.textContent = movies.forecast.forecastday[0].astro.sunset
+
+  const nextDayImg = document.querySelector('.ndImage')
+  const ndTemp = document.querySelector('.ndTemp')
+  ndTemp.textContent = movies.forecast.forecastday[1].day.maxtemp_f
+  const ndimg = document.createElement('img')
+  ndimg.src = movies.forecast.forecastday[1].day.condition.icon
+  ndimg.style.height = '60px'
+  
+  nextDayImg.appendChild(ndimg)
+  const ndText = document.querySelector('.ndText')
+  ndText.textContent = movies.forecast.forecastday[1].day.condition.text
+
+  const dayAfterImg = document.querySelector('.daImage')
+  const daTemp = document.querySelector('.daTemp')
+  daTemp.textContent = movies.forecast.forecastday[2].day.maxtemp_f
+  const daimg = document.createElement('img')
+  daimg.src = movies.forecast.forecastday[2].day.condition.icon
+  daimg.style.height = '60px'
+  
+  dayAfterImg.appendChild(daimg)
+  const daText = document.querySelector('.daText')
+  daText.textContent = movies.forecast.forecastday[2].day.condition.text
+
+  locationName.appendChild(location)
+  cardBody.appendChild(mainImage)
+  cardBodyBox.appendChild(title)
+  cardBodyBox.appendChild(temp)
+  cardBody.appendChild(cardBodyBox)
+  cardBody.appendChild(tempBtn)
+  display.appendChild(cardBody) 
+}
+
 
 async function getWeather() {
   let selection = document.getElementById('selection').value
